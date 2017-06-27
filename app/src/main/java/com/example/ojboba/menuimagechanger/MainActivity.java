b608dfd9599fe02185ec434aa318d8e2086ad65c
jjvang <jjv4ko@gmail.com>
2017-06-27 16:25:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ojboba/menuimagechanger/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ojboba.menuimagechanger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuImage:
+                Toast.makeText(this, "YOU THINK YOU GOT THIS?", Toast.LENGTH_SHORT).show();
+                item.setIcon(R.drawable.chicken);
+                return true;
+        }
+        return true;
+    }
+
+}
