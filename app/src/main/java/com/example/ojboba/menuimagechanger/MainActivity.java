c969282f3f68fa7e69e4e0d76874a42e806b8f5a
jjvang <jjv4ko@gmail.com>
2017-06-28 09:12:38 -0700

    Picture switches along with screen color

--- app/src/main/java/com/example/ojboba/menuimagechanger/MainActivity.java
+++ app/src/main/java/com/example/ojboba/menuimagechanger/MainActivity.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean maybe = true;
+    View mainView;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainView = findViewById(R.id.activity_main);
+        text = (TextView)findViewById(R.id.textColor);
+        mainView.setBackgroundResource(R.color.colorPrimaryDark);
+
     }
 
     @Override
@@ -24,8 +34,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.menuImage:
-                Toast.makeText(this, "YOU THINK YOU GOT THIS?", Toast.LENGTH_SHORT).show();
-                item.setIcon(R.drawable.chicken);
+                if (maybe == true ){
+                    Toast.makeText(this, "U CHiCKEN?", Toast.LENGTH_SHORT).show();
+                    item.setIcon(R.drawable.chicken);
+                    mainView.setBackgroundResource(R.color.colorAccent);
+                    text.setBackgroundResource(R.color.colorAccent);
+                    maybe = false;
+                } else{
+                    Toast.makeText(this, "OINK OINK", Toast.LENGTH_SHORT).show();
+                    item.setIcon(R.drawable.pig);
+                    mainView.setBackgroundResource(R.color.colorPrimary);
+                    text.setBackgroundResource(R.color.colorPrimary);
+                    maybe = true;
+                }
+
                 return true;
         }
         return true;
